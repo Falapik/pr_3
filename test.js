ae7c245443901099ad85d87ca864fcd1c18f44ae
Kolya Kolya <kolya.dima.caca@mail.ru>
2023-11-08 10:03:40 +0300

    add second swiper

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   direction: "horizontal",
   slidesPerView: 1.2,
   spaceBetween: 20,
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -18,19 +16,17 @@ function toggleLinks() {
   let par = document.querySelectorAll(".repare-none");
   let link = document.querySelectorAll(".links");
 
-  console.log(buttonText);
-
   if (par[0].style.display === "none") {
     par.forEach((el) => {
       el.style.display = "block";
     });
     buttonIcon.classList.add("active");
-    buttonText.innerHTML = "Скрыть"; // Optionally, you can update the button label
+    buttonText.innerHTML = "Скрыть";
   } else {
     par.forEach((el) => {
       el.style.display = "none";
     });
-    buttonText.innerHTML = "Показать всё"; // Optionally, you can update the button label
+    buttonText.innerHTML = "Показать всё";
     buttonIcon.classList.remove("active");
   }
   if (link[0].style.display === "none") {
